3c0d6774af9ecf1cf230b84f1c595f8230aed686
77-kiki <lyq1377313260@163.com>
2025-03-25 17:06:48 +0800

    first save

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # model.load('yolo11n.pt') # 加载预训练权重,改进或者做对比实验时候不建议打开，因为用预训练模型整体精度没有很明显的提升
-    model = YOLO(model=r'yolo11s.yaml')
+    model = YOLO(model=r'yolo11s-module.yaml')
     model.train(data=r'VisDrone.yaml',
                 imgsz=640,
                 epochs=300,
